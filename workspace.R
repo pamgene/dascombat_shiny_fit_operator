004e13f26216fc17480ec2799fd0d1e0fb0f5ad3
wptmdoorn <wptmdoorn@gmail.com>
2021-12-29 09:13:33 +0000

    Shiny UI and server functionality+

--- workspace.R
+++ workspace.R
@@ -1,64 +1,291 @@
 library(shiny)
 library(tercen)
+library(tim) # tercen/tim
 library(dplyr)
 library(tidyr)
+library(reshape2)
+library(ggplot2)
+source("R/core.R")
+source("R/pgcombat.R")
+
 
 ############################################
+# http://localhost:5402/admin/w/55b87c4ed069d42cc8d1e1efde0155af/ds/8ae9d32e-2799-4e93-864a-a79e348cbddf
 #### This part should not be included in ui.R and server.R scripts
 getCtx <- function(session) {
-  ctx <- tercenCtx(stepId = "9b7619c7-4d66-49fa-9bb3-2b06209e58e4",
-                   workflowId = "f81d245ef22a2ff192ed2533a6002ec3")
+  ctx <- tercenCtx(stepId = "8ae9d32e-2799-4e93-864a-a79e348cbddf",
+                   workflowId = "55b87c4ed069d42cc8d1e1efde0155af")
   return(ctx)
 }
 ####
 ############################################
 
-ui <- shinyUI(fluidPage(
-  
-  titlePanel("Histogram"),
-  
+ui <- shinyUI(fluidPage(sidebarLayout(
   sidebarPanel(
-    sliderInput("plotWidth", "Plot width (px)", 200, 2000, 500),
-    sliderInput("plotHeight", "Plot height (px)", 200, 2000, 500),
+    checkboxInput("applymode", "Apply saved model", FALSE),
+    conditionalPanel(
+      condition = "!input.applymode",
+      checkboxInput("useref", "Use a reference batch", value = FALSE),
+      conditionalPanel(
+        condition = 'input.useref',
+        selectInput("refbatch", "Select reference variable", choices = list())
+      ),
+      selectInput("modeltype", "Type of model", choices =  c("L/S", "L")),
+      checkboxInput("returnlink", "Return link to Combat model", value = FALSE)
+    ),
+    conditionalPanel(
+      condition = "input.applymode",
+      selectInput("modlink",
+                  "Select factor containing the model link",
+                  choices = list())
+    ),
+    actionButton("done", "Done"),
+    verbatimTextOutput("status")
   ),
-  
-  mainPanel(
-    uiOutput("reacOut")
-  )
-  
-))
+  mainPanel(plotOutput("pca"))
+)))
 
-server <- shinyServer(function(input, output, session) {
-  
+server <- shinyServer(function(input, output, session, context) {
   dataInput <- reactive({
     getValues(session)
   })
   
-  output$reacOut <- renderUI({
-    plotOutput(
-      "main.plot",
-      height = input$plotHeight,
-      width = input$plotWidth
-    )
-  }) 
+  # output$body = renderUI({
+  #   sidebarLayout(
+  #     sidebarPanel(
+  #       checkboxInput("applymode", "Apply saved model", FALSE),
+  #       conditionalPanel(
+  #         condition = "!input.applymode",
+  #         checkboxInput("useref", "Use a reference batch", value = FALSE),
+  #         conditionalPanel(
+  #           condition = 'input.useref',
+  #           selectInput("refbatch", "Select reference variable", choices = list())
+  #         ),
+  #         selectInput("modeltype", "Type of model", choices =  c("L/S", "L")),
+  #         checkboxInput("returnlink", "Return link to Combat model", value = FALSE)
+  #       ),
+  #       conditionalPanel(
+  #         condition = "input.applymode",
+  #         selectInput(
+  #           "modlink",
+  #           "Select factor containing the model link",
+  #           choices = list()
+  #         )
+  #       ),
+  #       actionButton("done", "Done"),
+  #       verbatimTextOutput("status")
+  #     ),
+  #     mainPanel(plotOutput("pca"))
+  #   )
+  # })
   
-  output$main.plot <- renderPlot({
-    values <- dataInput()
-    data <- values$data$.y
-    hist(data)
+  #getRunFolderReactive = context$getRunFolder()
+  #getStepFolderReactive = context$getFolder()
+  #getDataReactive = context$getData()
+  
+  observe({
+    #getData = getDataReactive$value
+    getData = dataInput
+    if (is.null(getData))
+      return()
+    
+    #getRunFolder = getRunFolderReactive$value
+    #if (is.null(getRunFolder))
+    #  return()
+    
+    #getStepFolder = getStepFolderReactive$value
+    #if (is.null(getStepFolder))
+    #  return()
+    
+    bndata = getData()
+    df = bndata$data
+    
+    print('Looking for data')
+    print(df)
+    #if (!bndata$hasColors) {
+    #  stop("Need exactly 1 data color for the batch variable or model link")
+    #}
+    
+    #if (length(bndata$colorColumnNames) > 1) {
+    #  stop("Need exactly 1 data color for the batch variable or model link")
+    #}
+    
+    df = bndata$data
+    df$bv = as.factor(bndata$ctx$select(bndata$ctx$colors))
+    
+    lab = paste("Select reference batch from the values in",
+                bndata$ctx$colors)
+    updateSelectInput(session,
+                      "refbatch",
+                      label = lab,
+                      choices = levels(df$bv))
+    updateSelectInput(session, "modlink", choices = bndata$arrayColumnNames)
+    
+    lmodel = reactive({
+      return(input$modeltype == "L")
+    })
+    
+    comfit = reactive({
+      X0 = acast(df, .ri ~ .ci, value.var = ".y")
+      bv = acast(df,  .ri ~ .ci, value.var = ".color")[1, ]
+      bv = droplevels(factor(bv))
+      rowSeq = acast(df,  .ri ~ .ci, value.var = ".ri")[, 1]
+      colSeq = acast(df,  .ri ~ .ci, value.var = ".ci")[1, ]
+      dimnames(X0) = list(rowSeq = rowSeq, colSeq = colSeq)
+      cmod = pgCombat$new()
+      if (input$useref) {
+        cmod = cmod$fit(X0,
+                        bv,
+                        ref.batch = input$refbatch,
+                        mean.only = lmodel())
+      } else {
+        cmod = cmod$fit(X0, bv, mean.only = lmodel())
+      }
+      
+      return(cmod)
+    })
+    
+    modfile = reactive({
+      mfile = levels(factor(df[[input$modlink]]))
+      bFile = file.exists(mfile)
+      if (!any(bFile))
+        stop("Model link not found")
+      mfile = mfile[bFile]
+      if (length(mfile) > 1)
+        stop("Incorrect model link")
+      return(mfile)
+      
+    })
+    
+    comapply = reactive({
+      modlink = modfile()
+      X0 = acast(df, rowSeq ~ colSeq, value.var = "value")
+      bv = acast(df,  rowSeq ~ colSeq, value.var = "bv")[1, ]
+      bv = droplevels(factor(bv))
+      rowSeq = acast(df,  rowSeq ~ colSeq, value.var = "rowSeq")[, 1]
+      colSeq = acast(df,  rowSeq ~ colSeq, value.var = "colSeq")[1, ]
+      dimnames(X0) = list(rowSeq = rowSeq, colSeq = colSeq)
+      load(modlink)
+      Xc = aCom$apply(X0, bv)
+      dimnames(Xc) = dimnames(X0)
+      result = list(X0 = X0,
+                    Xc = Xc,
+                    batches = bv)
+    })
+    
+    settingsTable = reactive({
+      if (!input$applymode) {
+        settings = data.frame(
+          setting = c("applymode", "useref", "refbatch", "modeltype"),
+          value  = c(
+            input$applymode,
+            input$useref,
+            input$refbatch,
+            input$modeltype
+          )
+        )
+      } else{
+        settings = data.frame(setting = "applymode", value = input$applymode)
+      }
+      return(settings)
+    })
+    
+    output$pca = renderPlot({
+      if (!input$applymode) {
+        aCom = comfit()
+      } else {
+        aCom = comapply()
+      }
+      
+      iPca = prcomp(t(aCom$X0))
+      fPca = prcomp(t(aCom$Xc))
+      pcaresi = data.frame(
+        PC1 = iPca$x[, 1],
+        PC2 = iPca$x[, 2],
+        bv = aCom$batches,
+        stage = "before"
+      )
+      pcaresf = data.frame(
+        PC1 = fPca$x[, 1],
+        PC2 = fPca$x[, 2],
+        bv = aCom$batches,
+        stage = "after"
+      )
+      pcares = rbind(pcaresi, pcaresf)
+      prt = ggplot(pcares, aes(
+        x = PC1 ,
+        y = PC2,
+        colour = bv
+      )) + geom_point()
+      prt = prt + facet_wrap( ~ stage, scales = "free")
+      return(prt)
+    })
+    
+    output$status = renderText({
+      isolate({
+        bLink = input$returnlink
+      })
+      if (input$done > 0) {
+        if (!input$applymode) {
+          aCom = comfit()
+        } else {
+          aCom = comapply()
+        }
+        Xc = aCom$Xc
+        dfXc = melt(Xc, value.name = "CmbCor")
+        dfXc$rowSeq = as.double(dfXc$rowSeq)
+        dfXc$colSeq = as.double(dfXc$colSeq)
+        if (!bLink | input$applymode) {
+          mdf = data.frame(
+            labelDescription = c("rowSeq", "colSeq", "CmbCor"),
+            groupingType = c("rowSeq", "colSeq", "QuantitationType")
+          )
+          result = AnnotatedData$new(data = dfXc, metadata = mdf)
+        } else {
+          #modellink = file.path(getRunFolder(), "modellink.RData")
+          #save(file = modellink, aCom)
+          #dfXc = data.frame(dfXc, modellink = as.character(modellink))
+          #mdf = data.frame(
+          #  labelDescription = c("rowSeq", "colSeq", "CmbCor", "modellink"),
+          #  groupingType = c("rowSeq", "colSeq", "QuantitationType", "Array")
+          #)
+          #result = AnnotatedData$new(data = dfXc, metadata = mdf)
+          print('Saving data and model...')
+          
+          # serialize data and return back
+          res <- get_serialized_result(
+            df = bndata$data,
+            object = aCom,
+            object_name = "dascombat_model",
+            ctx = bndata$ctx
+          )
+          
+          bndata$ctx$save(res)
+          
+          print('Saved data and model...')
+          
+        }
+        #settings = settingsTable()
+        #save(file = file.path(getRunFolder(), "runSettings.RData"), settings)
+        #context$setResult(result)
+        return("Done")
+      } else {
+        return(".")
+      }
+    })
   })
-  
 })
 
-getValues <- function(session){
+
+getValues <- function(session) {
   ctx <- getCtx(session)
   values <- list()
-
+  
   values$data <- ctx %>% select(.y, .ri, .ci) %>%
-    group_by(.ci, .ri) %>%
-    summarise(.y = mean(.y)) # take the mean of multiple values per cell
-
+    mutate(.color = ctx$select(ctx$colors[[1]]) %>% pull())
+  values$ctx <- ctx
+  
   return(values)
 }
 
-runApp(shinyApp(ui, server))  
+runApp(shinyApp(ui, server))
